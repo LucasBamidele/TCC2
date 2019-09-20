aa9893cc936119501a3064aadeda398c627bf0fe
Lucas Bamidele <bamidele.lucas@gmail.com>
2019-09-20 19:49:27 -0300

    just more files for backup just in case

---
+++ NeuralNet.py
@@ -0,0 +1,85 @@
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation, Dropout, Flatten
+from keras.optimizers import RMSprop
+from keras.layers.recurrent import LSTM
+from keras.callbacks import Callback
+from keras.layers import Conv1D, MaxPooling1D
+
+def cnn_model(num_players):
+	my_shape = ((2 + 2*num_players))
+	model = Sequential()
+
+	model.add(Conv1D(filters=64, kernel_size=1, padding='same',
+	                 input_shape=(my_shape,1)
+	                 ))
+	model.add(Activation('relu'))
+	
+	model.add(Conv1D(filters=64, kernel_size=1))
+	model.add(Activation('relu'))
+	model.add(Dropout(0.25))
+
+	model.add(MaxPooling1D(pool_size=1))
+	model.add(Dropout(0.25))
+
+	model.add(Conv1D(filters=64, kernel_size=1))
+	model.add(Activation('relu'))
+
+
+	model.add(Flatten())
+	
+	model.add(Dense(512))
+	model.add(Activation('relu'))
+	
+
+
+	model.add(Dense(512))
+	model.add(Activation('relu'))
+	model.add(Dropout(0.5))
+
+
+	myn = 50*13
+	model.add(Dense(myn))
+	model.add(Activation('softmax'))
+
+
+
+	rms = RMSprop()
+	model.compile(loss='categorical_crossentropy', optimizer=rms)
+	print(model.summary())
+	return model
+
+
+def neural_net_model(num_players):
+	model = Sequential()
+	#layer 1
+	my_shape = ((2 + 3*num_players))
+	model.add(Dense(
+		128,input_shape=(my_shape,)
+		))
+	model.add(Activation('relu'))
+	model.add(Dropout(0.2))
+
+	model.add(Dense(1024))
+	model.add(Activation('relu'))
+	model.add(Dropout(0.2))
+
+	model.add(Dense(512))
+	model.add(Activation('relu'))
+	model.add(Dropout(0.2))
+
+	model.add(Dense(512))
+	model.add(Activation('relu'))
+	model.add(Dropout(0.2))
+
+
+
+	myn = 50*13
+	model.add(Dense(myn))
+	#model.add(Activation('softmax'))
+	
+
+	rms = RMSprop()
+	model.compile(loss='categorical_crossentropy', optimizer=rms)
+
+
+	return model
