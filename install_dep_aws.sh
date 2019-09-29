427435a8fa716f36196c4c2bd3334be69c5a447b
LucasBamidele <bamidele.lucas@gmail.com>
2019-09-29 16:16:35 -0300

    aa

--- install_dep_aws.sh
+++ install_dep_aws.sh
@@ -13,16 +13,30 @@ dependency=(
     "sudo pip3 install pygame-menu"
     "sudo pip3 install tensorflow-gpu"
     "sudo pip3 install keras"
-    "wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.0.130-1_amd64.deb"
-    "sudo dpkg -i cuda-repo-ubuntu1804_10.0.130-1_amd64.deb"
-    "sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub"
-    "sudo apt-get update"
-    "wget http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64/nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb"
-    "sudo apt install ./nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb"
-    "sudo apt-get update"
-# Install NVIDIA driver
-    "sudo apt-get install --no-install-recommends nvidia-driver-418"
+#     "wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.0.130-1_amd64.deb"
+#     "sudo dpkg -i cuda-repo-ubuntu1804_10.0.130-1_amd64.deb"
+#     "sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub"
+#     "sudo apt-get update"
+#     "wget http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64/nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb"
+#     "sudo apt install ./nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb"
+#     "sudo apt-get update"
+# # Install NVIDIA driver
+#     "sudo apt-get install --no-install-recommends nvidia-driver-418"
 # Reboot. Check that GPUs are visible using the command: nvidia-smi
+
+#run the following:
+
+# sudo apt install cuda9.0 cuda-cublas-9-0 cuda-cufft-9-0 cuda-curand-9-0 \
+#     cuda-cusolver-9-0 cuda-cusparse-9-0 libcudnn7=7.2.1.38-1+cuda9.0 \
+#     libnccl2=2.2.13-1+cuda9.0 cuda-command-line-tools-9-0
+
+# wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
+# sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
+# sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+# sudo add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
+# sudo apt-get update
+# sudo apt-get -y install cuda
+
 )
 
 result(){
